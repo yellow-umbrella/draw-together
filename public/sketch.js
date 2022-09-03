1d7052adebf23267c5e9ab3218d8fdf8e7cd4c41
Daria Niemkevych <daria.niemkevych@gmail.com>
2022-09-03 12:35:49 +0300

    add simple use of sockets

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,15 @@
+var socket;
+
 function setup() {
     createCanvas(200, 200);
+
+    socket = io.connect('http://localhost:3000');
+    background(51); 
 }
 
 function draw() {
-    background(51);  
+    if (mouseIsPressed) {
+        noStroke();
+        circle(mouseX, mouseY, 5);
+    } 
 }
