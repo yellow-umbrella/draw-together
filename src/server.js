9012458bd6a71ee60cc3857034cf557b958ded87
Alexandr Litvin <klesh308@gmail.com>
2022-09-06 17:17:37 +0300

    server.js refactor - no spaghetti code - ConsoleLog class

--- src/server.js
+++ src/server.js
@@ -19,97 +19,175 @@ const timeToClose = 1000 * 60 * 60; // 1 hour in milliseconds
 
 function newConnection(socket) {
     let roomId = socket.id;
-    console.log('new connection:', socket.id);
+    ConsoleLog.newConnection(socket);
 
     socket.on('room', (data) => {
-        if (!rooms.has(data)) {
-            roomId = crypto.randomBytes(9).toString('base64');
-            rooms.set(roomId, {
-                users: new Set([socket.id]),
-                lines: new Array(),
-                background: null,
-                timeout: null,
-            });
-            socket.join(roomId);
-            socket.emit('path', '/' + roomId);
-            console.log(
-                socket.id,
-                'connected to',
-                roomId,
-                'with',
-                rooms.get(roomId).users.size,
-                'users'
-            );
-        } else {
-            roomId = data;
-            rooms.get(roomId).users.add(socket.id);
-            if (rooms.get(roomId).timeout != null) {
-                clearTimeout(rooms.get(roomId).timeout);
-                rooms.get(roomId).timeout = null;
-            }
-            socket.join(roomId);
-            console.log(
-                socket.id,
-                'connected to',
-                roomId,
-                'with',
-                rooms.get(roomId).users.size,
-                'users'
-            );
-            socket.emit('canvas', {
-                lines: rooms.get(roomId).lines,
-                background: rooms.get(roomId).background,
-            });
-            console.log('sent lines');
-        }
-        console.log('rooms: ' + rooms.size);
+        roomId = roomProcess(socket, data);
     });
 
     socket.on('disconnecting', () => {
-        if (rooms.has(roomId)) {
-            rooms.get(roomId).users.delete(socket.id);
-            if (rooms.get(roomId).users.size == 0) {
-                console.log('closing room', roomId);
-                rooms.get(roomId).timeout = setTimeout(
-                    deleteRoom,
-                    timeToClose,
-                    roomId
-                );
-            }
-        }
+        disconnectUser(roomId);
     });
 
     // receiving info about new line from client and sending it to all other clients
     socket.on('draw', (data) => {
-        if (rooms.has(roomId)) {
-            rooms.get(roomId).lines.push(data);
-        }
-        io.to(roomId).emit('draw', data);
+        draw(roomId, data);
     });
 
     socket.on('clearAll', () => {
-        if (rooms.has(roomId)) {
-            rooms.get(roomId).lines = [];
-        }
-        socket.to(roomId).emit('clearAll');
+        clearAllStuff(socket, roomId);
     });
 
     socket.on('background', (data) => {
-        console.log('background changed');
-        if (rooms.has(roomId)) {
-            rooms.get(roomId).background = data;
+        changeBackground(roomId, data);
+        redrawLines(roomId, data);
+    });
+}
+
+function disconnectUser(roomId) {
+    if (rooms.has(roomId)) {
+        rooms.get(roomId).users.delete(socket.id);
+
+        if (rooms.get(roomId).users.size == 0) {
+            ConsoleLog.closingRoom(roomId);
+
+            rooms.get(roomId).timeout = setTimeout(
+                deleteRoom,
+                timeToClose,
+                roomId
+            );
         }
-        io.to(roomId).emit('canvas', {
-            lines: rooms.get(roomId).lines,
-            background: data,
-        });
+    }
+}
+
+function draw(roomId, data) {
+    if (rooms.has(roomId)) {
+        rooms.get(roomId).lines.push(data);
+    }
+
+    io.to(roomId).emit('draw', data);
+}
+
+function clearAllStuff(socket, roomId) {
+    if (rooms.has(roomId)) {
+        rooms.get(roomId).lines = [];
+    }
+
+    socket.to(roomId).emit('clearAll');
+}
+
+function changeBackground(roomId, data) {
+    ConsoleLog.backgroundChange();
+
+    if (rooms.has(roomId)) {
+        rooms.get(roomId).background = data;
+    }
+}
+
+function redrawLines(roomId, data) {
+    io.to(roomId).emit('canvas', {
+        lines: rooms.get(roomId).lines,
+        background: data,
     });
 }
 
 function deleteRoom(roomId) {
     if (rooms.has(roomId)) {
         rooms.delete(roomId);
-        console.log('close room', roomId);
+
+        ConsoleLog.closeRoom(roomId);
+        ConsoleLog.roomsCount();
+    }
+}
+
+function roomExsists(data) {
+    return rooms.has(data);
+}
+
+function stopTimeOut(roomId) {
+    if (rooms.get(roomId).timeout != null) {
+        clearTimeout(rooms.get(roomId).timeout);
+        rooms.get(roomId).timeout = null;
+    }
+}
+
+function addUserToRoom(socket, roomId) {
+    rooms.get(roomId).users.add(socket.id);
+    socket.join(roomId);
+    ConsoleLog.userJoin(socket, roomId);
+}
+
+function loadCanvasToUser(socket, roomId) {
+    socket.emit('canvas', {
+        lines: rooms.get(roomId).lines,
+        background: rooms.get(roomId).background,
+    });
+    ConsoleLog.sentLines();
+}
+
+function createRoom(socket, roomId) {
+    rooms.set(roomId, {
+        users: new Set([socket.id]),
+        lines: new Array(),
+        background: null,
+        timeout: null,
+    });
+
+    socket.join(roomId);
+    socket.emit('path', '/' + roomId);
+    ConsoleLog.userJoin(socket, roomId);
+}
+
+function roomProcess(socket, data) {
+    let roomId;
+
+    if (roomExsists(data)) {
+        roomId = data;
+        stopTimeOut(roomId);
+        addUserToRoom(socket, roomId);
+        loadCanvasToUser(socket, roomId);
+    } else {
+        roomId = crypto.randomBytes(9).toString('base64');
+        createRoom(socket, roomId);
+    }
+    ConsoleLog.roomsCount();
+
+    return roomId;
+}
+
+class ConsoleLog {
+    static userJoin(socket, roomId) {
+        console.log(
+            socket.id,
+            'connected to',
+            roomId,
+            'with',
+            rooms.get(roomId).users.size,
+            'users'
+        );
+    }
+
+    static newConnection(socket) {
+        console.log('new connection:', socket.id);
+    }
+
+    static roomsCount() {
         console.log('rooms:', rooms.size);
+    }
+
+    static sentLines() {
+        console.log('sent lines');
+    }
+
+    static closingRoom(roomId) {
+        console.log('closing room', roomId);
+    }
+
+    static backgroundChange() {
+        console.log('background changed');
+    }
+
+    static closeRoom(roomId) {
+        console.log('close room', roomId);
     }
 }
